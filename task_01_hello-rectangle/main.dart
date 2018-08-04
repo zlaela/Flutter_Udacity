46903b1eb3f6237f024e429586f4e7f6d8e5480a
zlaela <zlaela@users.noreply.github.com>
2018-08-04 08:32:56 -0500

    Add files via upload

---
+++ task_01_hello-rectangle/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.blueGrey,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text("Hello!", style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  print('Hello!');
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello, New Rectangle!")
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
